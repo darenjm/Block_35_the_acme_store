502633b8d23c3b835dc29107ac3f3c8051f70719
Daren Morreale <daren@givelegacy.com>
2025-04-22 19:39:45 -0500

    added final two endpoints

--- server/index.js
+++ server/index.js
@@ -46,3 +46,21 @@ server.get("/api/users/:id/favorites", async (req, res, next) => {
     next(error);
   }
 });
+
+server.post("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    const skill = await createUserSkill(req.params.id, req.body.skill_id);
+    res.send(skill);
+  } catch (error) {
+    next(error);
+  }
+});
+
+server.delete("/api/users/:user_id/favorites/:id", async (req, res, next) => {
+  try {
+    await destroyUserSkill(req.params.id, req.params.user_id);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
